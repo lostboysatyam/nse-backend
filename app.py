ad14721d731f4ecd377cbd959bfb28059048eea7
Satyam Sagar <86399101+lostboysatyam@users.noreply.github.com>
2025-10-07 10:11:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -108,4 +108,4 @@ if __name__ == "__main__":
     print("Starting refresh thread...")
     threading.Thread(target=refresh_cache_loop, daemon=True).start()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False
+    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
